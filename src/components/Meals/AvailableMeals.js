4f97b9c794cfe3024dba9b1ac83c9928b5573827
YeshwanthKare <yesh.kare@gmail.com>
2022-09-03 17:30:27 +0530

    ADD: fecthed available meals from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 function AvailableMeals() {
-  const MealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      let response = await fetch(
+        "https://react-http-c4544-default-rtdb.firebaseio.com/meals.json",
+        { method: "GET" }
+      );
+      let responseData = await response.json();
+
+      let loadedMeals = [];
+      for (let key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, [meals]);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  const MealsList = meals.map((meal) => (
     <MealItem
       name={meal.name}
       description={meal.description}
@@ -44,6 +51,7 @@ function AvailableMeals() {
   return (
     <section className={classes.meals}>
       <Card>
+        {isLoading && <p>Loading...</p>}
         <ul>{MealsList}</ul>
       </Card>
     </section>
